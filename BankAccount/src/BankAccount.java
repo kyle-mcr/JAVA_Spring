0c4a7eb76631bf8f9af3a71e7a05d534d1832227
Kyle McReynolds <Kyle@Kyles-MacBook-Air.local>
2019-10-30 17:31:09 -0700

    BANK

--- BankAccount/src/BankAccount.java
+++ BankAccount/src/BankAccount.java
@@ -1,17 +1,39 @@
 
 public class BankAccount {
-	private String accountNumber;
+	private Double accountNumber;
 	private Double checkingBalance;
 	private Double savingsBalance;
 	public static int numberOfAccounts = 0;
 	public static int totalMoney = 0;
+	public static Double  randomAccount = Math.random()*100 ;
 	public BankAccount(String accountParam, Double checkingParam, Double savingsParam) {
-		accountNumber = accountParam;
+		accountNumber = makeNumber();
 		checkingBalance = checkingParam;
 		savingsBalance = savingsParam;
 		numberOfAccounts++;
 		totalMoney += checkingBalance + savingsBalance;
 	}
-	
-	
+	private Double makeNumber() {
+		Double x;
+		x = (Double)(Math.random()*10);
+		return x;
+	}
+	public Double checkBalance() {
+		return checkingBalance;
+	}
+	public Double saveBalance() {
+		return savingsBalance;
+	}
+	public void deposit(double num) {
+		checkingBalance += num;
+		totalMoney += num;
+	}
+	public void depositSave(double num) {
+		savingsBalance += num;
+		totalMoney += num;
+	}
+	public void withdrawl(double num) {
+		savingsBalance -= num;
+		totalMoney -= num;
+	}
 }
--- BankAccount/src/BankAccountTest.java
+++ BankAccount/src/BankAccountTest.java
@@ -5,6 +5,7 @@ public class BankAccountTest {
 		BankAccount a = new BankAccount("1234", 100.25, 44.20);
 		BankAccount b = new BankAccount("5678", 200.44, 23.24);
 		System.out.println(BankAccount.numberOfAccounts);
+		System.out.println(BankAccount.totalMoney);
 	}
 
 }
